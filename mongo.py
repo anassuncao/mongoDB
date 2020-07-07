6201182f8e8a42768955fc19dd484634dfa85dbc
anassuncao <ana.v.assuncao@gmail.com>
2020-07-07 11:54:11 +0000

    after all lessons complete, before the mini porject

--- mongo.py
+++ mongo.py
@@ -1,7 +1,9 @@
 import pymongo
 import os
+if os.path.exists("env.py"):
+    import env
 
-MONGODB_URI = os.getenv("MONGO_URI")
+MONGODB_URI = os.environ.get("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
@@ -19,7 +21,27 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
+# to insert one record to our database:
+"""new_doc = {'first': 'douglas', 'last': 'adams', 'dob': '11/03/1952', 'hair_colour': 'gray', 'occupation': 'writer', 'nationality': 'english'}"""
+
+# to insert several records to our database:
+"""new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 'gender': 'm', 'hair_colour': 'not much', 'occupation': 'writer', 'nationality': 'english'}, {'first': 'george', 'last': 'rr martin', 'dob': '20/09/1948', 'gender': 'm', 'hair_colour': 'white', 'occupation': 'writer', 'nationality': 'american'}]
+
+coll.insert_many(new_docs)"""
+
+# to find specifi data:
+"""documents = coll.find({'first': 'douglas'})"""
+
+# to delete data:
+"""coll.remove({'first': 'douglas'})"""
+
+# to update data in our database
+"""coll.update_one({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})"""
+
+# to update several records in our database
+coll.update_many({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+
+documents = coll.find({'nationality': 'american'})
 
 for doc in documents:
     print(doc)
